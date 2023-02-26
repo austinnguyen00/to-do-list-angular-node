ba9582828dad0c83e49ebb33fd478b2a7238d2f1
Austin <ndkhiem3010@gmail.com>
2023-02-26 20:50:43 +0800

    Adding new method in user service to retrieve current user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../shared/models/User';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,16 @@ import { User } from '../shared/models/User';
 export class UserService {
   private apiUrl = 'http://localhost:5000/api/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public auth: AuthService) { }
 
   getUsers(): Observable<User[]> {
     // Retrieve all users in our application
     return this.http.get<User[]>(this.apiUrl);
+  }
+
+  getCurrentUser(): Observable<any> {
+    // auth.user$ return an observable where we can subscribe to
+    // and retrieve current authenticated user information
+    return this.auth.user$;
   }
 }
