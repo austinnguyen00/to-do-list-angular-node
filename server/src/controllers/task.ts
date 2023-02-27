070241b1e9ae5ccba131f9ea70588e0e3a38f6c1
Austin <ndkhiem3010@gmail.com>
2023-02-28 00:39:34 +0800

    Update task controller queries for the changing of postgresql database

--- server/src/controllers/task.ts
+++ server/src/controllers/task.ts
@@ -1,48 +1,54 @@
 import { Request, Response } from 'express';
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  host: process.env.INSTANCE_HOST, // e.g. '127.0.0.1'
-  port: process.env.DB_PORT, // e.g. '5432'
-  user: process.env.DB_USER, // e.g. 'my-user'
-  password: process.env.DB_PASS, // e.g. 'my-user-password'
-  database: process.env.DB_NAME, // e.g. 'my-database'
-})
+const postgres = require('postgres');
+require('dotenv').config();
 
-const getTasks = (req: Request, res: Response) => {
+const URL = `${process.env.DB_STRING}?options=project%3D${process.env.ENDPOINT_ID}`;
+const sql = postgres(URL, { ssl: 'require' });
+
+const getTasks = async (req: Request, res: Response) => {
   // Query to retrieve all tasks in the tasks table
-  pool.query('SELECT * FROM Tasks', (error: Error, results: any) => {
-    if (error) {
-      throw error
-    }
-    console.log('Results:', results.rows)
-    res.status(200).json(results.rows)
-  })
+  // pool.query('SELECT * FROM Tasks', (error: Error, results: any) => {
+  //   if (error) {
+  //     throw error
+  //   }
+  //   console.log('Results:', results.rows)
+  //   res.status(200).json(results.rows)
+  // })
+  const tasks = await sql`SELECT * FROM tasks`;
+  console.log('Result:', tasks);
+  return res.status(200).json(tasks)
 }
 
-const addTask = (req: Request, res: Response) => {
-  console.log('Req', req.body);
-  // Insert query to add new task with given text and reminder
-  pool.query(`INSERT INTO tasks ("text", "reminder", "username") VALUES ($1, $2, $3)`, [req.body.text, req.body.reminder, req.body.username],
-    (error: Error, results: any) => {
-      if (error) {
-        throw error
-      }
-      console.log('Added:', results)
-      res.status(200).json(req.body)
-    })
+const addTask = async (req: Request, res: Response) => {
+  // // Insert query to add new task with given text and reminder
+  // pool.query(`INSERT INTO tasks ("text", "reminder", "username") VALUES ($1, $2, $3`, [req.body.text, req.body.reminder, req.body.username],
+  //   (error: Error, results: any) => {
+  //     if (error) {
+  //       throw error
+  //     }
+  //     console.log('Added:', results)
+  //     res.status(200).json(req.body)
+  //   })
+  const task = await sql`INSERT INTO tasks ("text", "reminder", "username") VALUES (${req.body.text}, ${req.body.reminder}, ${req.body.username})`;
+  console.log('Result:', task);
+  return res.status(200).json(req.body)
+
 }
 
-const getTasksByUser = (req: Request, res: Response) => {
+const getTasksByUser = async (req: Request, res: Response) => {
   // Query to retrieve tasks of the given user in the URL
-  const username = req.params.username;
-  pool.query(`SELECT * FROM Tasks WHERE username = $1`, [username], (error: Error, results: any) => {
-    if (error) {
-      throw error
-    }
-    console.log('Results:', results.rows)
-    res.status(200).json(results.rows)
-  })
+  // const username = req.params.username;
+  // pool.query(`SELECT * FROM Tasks WHERE username = $1`, [username], (error: Error, results: any) => {
+  //   if (error) {
+  //     throw error
+  //   }
+  //   console.log('Results:', results.rows)
+  //   res.status(200).json(results.rows)
+  // })
+  const tasks = await sql`SELECT * FROM tasks WHERE username = ${req.params.username}`;
+  console.log('Result:', tasks);
+  return res.status(200).json(tasks)
 }
 
 module.exports = {
