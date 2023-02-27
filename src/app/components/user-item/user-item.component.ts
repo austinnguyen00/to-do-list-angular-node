9adfb0d008a4d03a03ec208778659c9be8d55c26
Austin <ndkhiem3010@gmail.com>
2023-02-27 18:53:15 +0800

    Update user-item component to show user's tasks

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { User } from '../../shared/models/User'
+import { Task } from '../../shared/models/Task';
 
 @Component({
   selector: 'app-user-item',
@@ -8,5 +10,19 @@ import { User } from '../../shared/models/User'
 })
 export class UserItemComponent {
   @Input() user: User;
+  showTasks: boolean = false;
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasksByUser(this.user.nickname).subscribe(tasks => this.tasks = tasks);
+  }
+
+  // When user click arrow down button then show task
+  // When click again then hide task
+  toggleTasks = (): void => {
+    this.showTasks = !this.showTasks;
+  }
 }
