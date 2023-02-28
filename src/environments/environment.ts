c69243c6af78247cce20bb027f70d20818b59e63
Austin <ndkhiem3010@gmail.com>
2023-02-28 19:14:10 +0800

    Update environment api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://to-do-list-angular-node.web.app/api'
+  apiUrl: 'https://us-central1-to-do-list-angular-node-b8f78.cloudfunctions.net/app/api'
 };
