aae890b78fd983f65e278e7a8886497e895235e4
Austin <ndkhiem3010@gmail.com>
2023-02-28 19:06:29 +0800

    Fix minor environment setting in task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from 'src/app/shared/models/Task';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
